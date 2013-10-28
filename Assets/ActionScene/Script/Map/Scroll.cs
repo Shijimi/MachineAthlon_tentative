75460ee350770e836fc4310acebfff7da9ed460e
Shijimi <nishida@st.asojuku.ac.jp>
2013-10-28 23:49:02 +0900

    アイテム・障害物の消滅位置を修正

--- Assets/ActionScene/Script/Map/Scroll.cs
+++ Assets/ActionScene/Script/Map/Scroll.cs
@@ -14,21 +14,24 @@ public class Scroll : MonoBehaviour
 {
     [SerializeField]
     private GameObject cPlayer;
+	
+	//オブジェクトが消滅するときのZ座標.
+	private const float OBJ_REMOVE_POS_Z = -60;
 
 	// Use this for initialization
 	void Start ()
     {
+		//プレイヤーのゲームオブジェクトを取得する
         cPlayer = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+		//プレイヤーの速さに応じて座標を移動させる.
         gameObject.transform.position -= new Vector3(0.0f, 0.0f, cPlayer.GetComponent<Player>().GetSpeed());
-
-        if (gameObject.transform.position.z < -50)
-        {
-            Destroy(gameObject);
-        }
+		
+		//画面外に出た場合はオブジェクトを削除する.
+        if (gameObject.transform.position.z < OBJ_REMOVE_POS_Z){Destroy(gameObject);}
 	}
 }
