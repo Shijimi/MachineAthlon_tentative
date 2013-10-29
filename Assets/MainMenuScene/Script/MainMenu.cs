c137c6cff5ff4d6674d10da4d4c4fa541e961f44
MERUHEN <boc.ktm.pg@gmail.com>
2013-10-29 22:55:47 +0900

    フェード追加

--- Assets/MainMenuScene/Script/MainMenu.cs
+++ Assets/MainMenuScene/Script/MainMenu.cs
@@ -3,16 +3,34 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+	//	フェードの時間を確保
+	int		m_fadeTime;
+	//	フェード中か？
+	bool	m_fadeFlg;
+	
     void Start()
     {
+		m_fadeTime	=	60;
+		Fade.FadeOut();
+		m_fadeFlg	=	false;
     }
+	
+	void Update ()
+    {
+		if( m_fadeFlg )
+			m_fadeTime--;
+		
+		if( m_fadeTime <= 0 )
+	           //アクションシーンを開始する
+     	       Application.LoadLevel("ActionScene");
+	}
 
     void OnGUI()
     {
         if (GUI.Button(new Rect(20,Screen.height/2, 300, 40), "ゲームスタート"))
         {
-           //アクションシーンを開始する
-            Application.LoadLevel("ActionScene");
+			Fade.FadeIn();
+			m_fadeFlg	=	true;
         }
 
         if (GUI.Button(new Rect(20, Screen.height / 2 + 60,300, 40), "操作説明"))
--- Assets/ResultScene/Script/ResultMenu.cs
+++ Assets/ResultScene/Script/ResultMenu.cs
@@ -3,22 +3,48 @@ using System.Collections;
 
 public class ResultMenu : MonoBehaviour
 {
+	int		m_fadeTime;		//	フェードの時間を確保
+	int		m_next;			//	次のシーン
+	bool	m_fadeFlg;		//	フェード中か？
+	const	int	NEXT_ACTION	=	1;		
+	
     void Start()
     {
-    }
+		m_fadeTime	=	60;
+		Fade.FadeOut();
+		m_fadeFlg	=	false;
+		m_next		=	0;
+	}
+	
+	void Update ()
+    {
+		if( m_fadeFlg )
+			m_fadeTime--;
+		
+		if( m_fadeTime <= 0 )
+		{
+			if( m_next == NEXT_ACTION )
+	           //	アクションシーンを開始する.
+     	       Application.LoadLevel("ActionScene");
+			else
+				//	タイトルを表示する.
+	            Application.LoadLevel("MainMenuScene"); 
+		}
+	}
 
     void OnGUI()
     {
         if (GUI.Button(new Rect(20, Screen.height / 2, 300, 40), "リトライ"))
         {
-            //アクションシーンを開始する
-            Application.LoadLevel("ActionScene");
+			Fade.FadeIn();
+			m_fadeFlg	=	true;
+			m_next	=	NEXT_ACTION;
         }
 
         if (GUI.Button(new Rect(20, Screen.height / 2 + 60, 300, 40), "タイトルに戻る"))
         {
-            //操作説明を表示する
-            Application.LoadLevel("MainMenuScene"); 
+			Fade.FadeIn();
+			m_fadeFlg	=	true;
         }
     }
 }
