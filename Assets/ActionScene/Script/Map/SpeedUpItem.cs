d127572bd2b6d22fa95672012dc77fb04e37f68a
Shijimi <nishida@st.asojuku.ac.jp>
2014-01-17 17:17:35 +0900

    加速アイテムに強調アニメーションを追加

---
+++ Assets/ActionScene/Script/Map/SpeedUpItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedUpItem : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //点滅アニメーション
+        float fColor = 1.5f - Mathf.Sin(Time.time * 10.0f);
+        renderer.material.color = new Color(fColor, fColor, fColor);
+	}
+}
