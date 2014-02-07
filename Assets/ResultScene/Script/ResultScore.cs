d196c7135ed9e7ed6697f3580aa3fbdc1586fa83
Shijimi <nishida@st.asojuku.ac.jp>
2014-02-07 18:38:24 +0900

    ステージごとのスコア表示(バグあり)
    スコアがfloat型のため切り捨てで合計が合わない
    エフェクト完成後ActionScene修正予定

---
+++ Assets/ResultScene/Script/ResultScore.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultScore : MonoBehaviour
+{
+    [SerializeField]
+    private string m_strTag;
+
+    private ScoreBoard m_cScoreBoard;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //総移動距離を取得
+        uint nScore = (uint)GameObject.Find("DataManager").GetComponent<DataManager>().GetData(m_strTag);
+
+        m_cScoreBoard = gameObject.GetComponent<ScoreBoard>();
+
+        //5桁のスコアボードの作成
+        m_cScoreBoard.Create(5);
+
+        m_cScoreBoard.Set((uint)nScore);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
