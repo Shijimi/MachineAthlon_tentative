b2a2d40a1c21bf197cac985d9e829bcb69e5f789
Shijimi <nishida@st.asojuku.ac.jp>
2014-02-09 02:39:52 +0900

    ActionSceneのScoreBoard追加

---
+++ Assets/ActionScene/Script/GameSystem/ActionScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionScore : MonoBehaviour
+{
+    private ScoreBoard m_cScoreBoard;
+    private Player m_cPlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //総移動距離を取得
+        m_cPlayer = GameObject.Find("Player").GetComponent<Player>();
+
+        m_cScoreBoard = gameObject.GetComponent<ScoreBoard>();
+
+        //5桁のスコアボードの作成
+        m_cScoreBoard.Create(5);
+
+        m_cScoreBoard.Set(8888);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_cScoreBoard.Set((uint)m_cPlayer.GetDistance());
+	}
+}
